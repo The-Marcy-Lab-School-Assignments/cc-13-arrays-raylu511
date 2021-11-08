caced058953c0bf2c0f31f867163a66c8d827821
Ray Lu <raylu511@gmail.com>
2021-11-08 15:04:32 +0000

    Finished code challenge

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //write your code here
+
+
+
+
+function averageInArray(numArray) {
+// declare a function named averageInArray that takes in an array of numbers
+// initiliaze sum = 0 
+// iterate through array and get sum 
+// return sum / length of array
+  let sum = 0; 
+  for (let i = 0 ; i < numArray.length; i++) {
+    sum += numArray[i];
+  }
+  return sum/numArray.length; 
+}
+
+console.log(averageInArray([2,4,5,7,8]))    // 5.2
+console.log(averageInArray([2,2,5,10]))     // 4.75
+console.log(averageInArray([2,2,2,2,2]))    // 2
